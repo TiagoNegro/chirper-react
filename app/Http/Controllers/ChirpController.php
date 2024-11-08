a654ee93eb20513fa22058322d5d28771bfd2de1
tnegro <tiago.negro1@hotmail.com>
2024-11-07 22:53:05 -0300

    Criação do metodo de salvar os chirps, revisão no formulario do chirp e atualização da migration chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -30,9 +31,13 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
